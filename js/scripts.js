1c3894c01c4dd3bfa845661eae00e6ecbb8f6f07
Will Saunders <willsaunders1014@gmail.com>
2017-08-08 15:09:50 -0700

    changed img folders, added meta description

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
+var active = 0;
 $(document).ready(function(){
 	$('#nav').find('li').hover(function(){
 		$('#nav').find('li').eq(0).removeClass('hover');
 		$(this).addClass('hover');
 	}, function(){
-		$(this).removeClass('hover');
+		$('#nav').find('li').removeClass('hover');
 	}).click(function(){
 		var text = $(this).find('.text').html().split(' ').join('-');
-		console.log(text);
 		$('#content .wrapper > div').hide();
+		$('#content')[0].scrollTop = 0;
+		active = $(this).index();
 		$('#'+text).show();
+
 	});
+	$('#nav').on('mouseleave', function(){
+		$('#nav').find('li').eq(active).addClass('hover');
+	})
 })
